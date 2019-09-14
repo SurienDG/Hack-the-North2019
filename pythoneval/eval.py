5e269b075f30a0da861111053e5b54afb824ede7
Miguel Caringal <Miguel.Caringal@gmail.com>
2019-09-13 23:43:04 -0400

    initial testing

---
+++ pythoneval/eval.py
@@ -0,0 +1,28 @@
+from radon.complexity import cc_rank, cc_visit
+from radon.metrics import mi_visit, mi_rank
+from radon.raw import analyze
+#cc_rank(4), cc_rank(9), cc_rank(14), cc_rank(23)
+#('A', 'B', 'C', 'D')
+
+# Metrics that are usable: Maintainability Index
+
+teststring = ('''
+
+
+''')
+
+def my_function():
+  print("Hello from a function")
+
+def factorial(n):
+    if n < 2: return 1
+    return n * factorial(n - 1)
+
+#print (analyze(teststring))
+
+#print (cc_visit(teststring))
+
+mivalue = (mi_visit(teststring,0))
+
+print (mivalue)
+print (mi_rank(mivalue))
